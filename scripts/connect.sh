e272a8791527adb4ae65cc8d970d8e3587846072
Sergio Castineyras <sergio.castineyras@globallogic.com>
2019-03-02 23:03:30 -0300

    bug fix

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 if [ -z "$CERT" ]
 then
-  echo $ANYCONNECT_PASSWORD|openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER -b --csd-wrapper=csd-wrapper.sh
+  echo $ANYCONNECT_PASSWORD|openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER -b --csd-wrapper=/root/csd-wrapper.sh --no-dtls
 else
-  echo $ANYCONNECT_PASSWORD|openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --servercert=$CERT -b --csd-wrapper=csd-wrapper.sh
+  echo $ANYCONNECT_PASSWORD|openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --servercert=$CERT -b --csd-wrapper=/root/csd-wrapper.sh --no-dtls
 fi
 
 sleep 5
 
-/usr/sbin/jdidvpn -f /etc/jdidvpn.conf -D
+/usr/sbin/danted -f /etc/jdidvpn.conf -D
 
 socat -d TCP4-LISTEN:3389,fork TCP4:$JUMP1:3389 &
 socat -d TCP4-LISTEN:3390,fork TCP4:$JUMP2:3389 &
